c0f2e58be271f647edf6a5cc3555c43e2f6ad4fb
junhyok7146 <junhyeok_an@naver.com>
2024-02-07 12:55:40 +0900

    2/7 오전

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,7 @@ if (ww>910) {
 }else{
     $("html").addClass("mobile")
 }
-
+ //==================================================================================   
 $('.slideinner').slick({
     autoplay: true, 
     autoplayspeed: 4000, 
@@ -21,44 +21,43 @@ $(this).removeClass("fa-pause").addClass("fa-play");
 $(".slideouter .plapau i").removeClass("fa-pause").addClass("fa-play");
 $(this).removeClass("fa-play").addClass("fa-pause");
 }
-
 });
-
+ //==================================================================================   
 $("#nav .depth1").on("mouseover mouseout",function(){
     if($("html").hasClass("pc")){
         $(this).find(".depth2").stop().slideToggle(200)
         $(".bgbox").stop().slideToggle(200)
     }
 });
-
+ //==================================================================================   
 $("#nav .depth1 > li").on("click",function(){
     if($("html").hasClass("mobile")){
         $(this).toggleClass("on")
         $(this).siblings().removeClass("on")
     }
 });
+ //==================================================================================   
 $(document).ready(function () {
     $("#header .row .menuopen").click(function () {
       $("#header #nav ul.depth1").toggleClass("on");
     });
   });
 
-
-$(document).ready(function(){
+  $(document).ready(function(){
+ //==================================================================================   
     $('#section .contact .position li').click(function(){
-        $(this).css({'background-color': '', 'color': '#444'});
-        $(this).find('img').css('filter', '');
-        $(this).css({'background-color': '#000', 'color': '#fff'});
-        $(this).find('img').css('filter', 'brightness(3000%)');
+        var backgroundColor = $(this).css('background-color');
+        var color = $(this).css('color');  
+        $(this).css({'background-color': color, 'color': backgroundColor});
+    });  
+    $('#section .contact .position li').click(function(){
+        var img = $(this).find('img'); 
+        var filterValue = img.css('filter');     
+        if (filterValue === 'none') {
+            img.css('filter', 'brightness(3000%)');
+        } else {
+            img.css('filter', 'none');
+        }
     });
 });
-
-
-// $(document).ready(function(){
-//     $('#section .contact .position li').click(function(){
-//         var backgroundColor = $(this).css('background-color');
-//         var color = $(this).css('color');  
-      
-//         $(this).css({'background-color': color, 'color': backgroundColor});
-//     });
-// });
+ //==================================================================================  
